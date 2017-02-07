0188bfbc60aed3f0667f7eec5b02b33f91c16eab
sam-price <samprice.ethos@gmail.com>
2017-02-07 20:04:24 +0000

    Added jsonb column for user settings

---
+++ db/migrate/20170207195030_add_settings_to_users.rb
@@ -0,0 +1,6 @@
+class AddSettingsToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :settings, :jsonb, null: false, default: {}
+    add_index :users, :settings, using: :gin
+  end
+end
