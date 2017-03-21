faa93242c2e546528a210e04a57ec773ebf4f1bb
sam-price <samprice.ethos@gmail.com>
2017-03-21 10:53:35 +0000

    cleanup edits

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,10 +18,10 @@
 //= require_tree .
 
 /*****
-* CONFIGURATION
+CONFIGURATION
 */
     //Main navigation
-    $.navigation = $('nav > ul.nav');
+  $.navigation = $('nav > ul.nav');
 
   $.panelIconOpened = 'icon-arrow-up';
   $.panelIconClosed = 'icon-arrow-down';
@@ -44,120 +44,122 @@
 /****
 * MAIN NAVIGATION
 */
+$(document).on('turbolinks:load', function() {
+  $(document).ready(function($){
 
-$(document).ready(function($){
+    // Add class .active to current link
+    $.navigation.find('a').each(function(){
 
-  // Add class .active to current link
-  $.navigation.find('a').each(function(){
+      var cUrl = String(window.location).split('?')[0];
 
-    var cUrl = String(window.location).split('?')[0];
-
-    if (cUrl.substr(cUrl.length - 1) == '#') {
-      cUrl = cUrl.slice(0,-1);
-    }
-
-    if ($($(this))[0].href==cUrl) {
-      $(this).addClass('active');
-
-      $(this).parents('ul').add(this).each(function(){
-        $(this).parent().addClass('open');
-      });
-    }
-  });
-
-  // Dropdown Menu
-  $.navigation.on('click', 'a', function(e){
-
-    if ($.ajaxLoad) {
-      e.preventDefault();
-    }
-
-    if ($(this).hasClass('nav-dropdown-toggle')) {
-      $(this).parent().toggleClass('open');
-      resizeBroadcast();
-    }
-
-  });
-
-  function resizeBroadcast() {
-
-    var timesRun = 0;
-    var interval = setInterval(function(){
-      timesRun += 1;
-      if(timesRun === 5){
-        clearInterval(interval);
+      if (cUrl.substr(cUrl.length - 1) == '#') {
+        cUrl = cUrl.slice(0,-1);
       }
-      window.dispatchEvent(new Event('resize'));
-    }, 62.5);
-  }
 
-  /* ---------- Main Menu Open/Close, Min/Full ---------- */
-  $('.navbar-toggler').click(function(){
+      if ($($(this))[0].href==cUrl) {
+        $(this).addClass('active');
 
-    if ($(this).hasClass('sidebar-toggler')) {
-      $('body').toggleClass('sidebar-hidden');
-      resizeBroadcast();
+        $(this).parents('ul').add(this).each(function(){
+          $(this).parent().addClass('open');
+        });
+      }
+    });
+
+    // Dropdown Menu
+    $.navigation.on('click', 'a', function(e){
+
+      if ($.ajaxLoad) {
+        e.preventDefault();
+      }
+
+      if ($(this).hasClass('nav-dropdown-toggle')) {
+        $(this).parent().toggleClass('open');
+        resizeBroadcast();
+      }
+
+    });
+
+    function resizeBroadcast() {
+
+      var timesRun = 0;
+      var interval = setInterval(function(){
+        timesRun += 1;
+        if(timesRun === 5){
+          clearInterval(interval);
+        }
+        window.dispatchEvent(new Event('resize'));
+      }, 62.5);
     }
 
-    if ($(this).hasClass('aside-menu-toggler')) {
-      $('body').toggleClass('aside-menu-hidden');
-      resizeBroadcast();
-    }
+    /* ---------- Main Menu Open/Close, Min/Full ---------- */
+    $('.navbar-toggler').click(function(){
 
-    if ($(this).hasClass('mobile-sidebar-toggler')) {
-      $('body').toggleClass('sidebar-mobile-show');
-      resizeBroadcast();
-    }
+      if ($(this).hasClass('sidebar-toggler')) {
+        $('body').toggleClass('sidebar-hidden');
+        resizeBroadcast();
+      }
+
+      if ($(this).hasClass('aside-menu-toggler')) {
+        $('body').toggleClass('aside-menu-hidden');
+        resizeBroadcast();
+      }
+
+      if ($(this).hasClass('mobile-sidebar-toggler')) {
+        $('body').toggleClass('sidebar-mobile-show');
+        resizeBroadcast();
+      }
+
+    });
+
+    $('.sidebar-close').click(function(){
+      $('body').toggleClass('sidebar-opened').parent().toggleClass('sidebar-opened');
+    });
+
+    /* ---------- Disable moving to top ---------- */
+    $('a[href="#"][data-top!=true]').click(function(e){
+      e.preventDefault();
+    });
 
   });
 
-  $('.sidebar-close').click(function(){
-    $('body').toggleClass('sidebar-opened').parent().toggleClass('sidebar-opened');
-  });
+  /****
+  * CARDS ACTIONS
+  */
 
-  /* ---------- Disable moving to top ---------- */
-  $('a[href="#"][data-top!=true]').click(function(e){
+  $(document).on('click', '.card-actions a', function(e){
     e.preventDefault();
-  });
 
-});
+    if ($(this).hasClass('btn-close')) {
+      $(this).parent().parent().parent().fadeOut();
+    } else if ($(this).hasClass('btn-minimize')) {
+      var $target = $(this).parent().parent().next('.card-block');
+      if (!$(this).hasClass('collapsed')) {
+        $('i',$(this)).removeClass($.panelIconOpened).addClass($.panelIconClosed);
+      } else {
+        $('i',$(this)).removeClass($.panelIconClosed).addClass($.panelIconOpened);
+      }
 
-/****
-* CARDS ACTIONS
-*/
-
-$(document).on('click', '.card-actions a', function(e){
-  e.preventDefault();
-
-  if ($(this).hasClass('btn-close')) {
-    $(this).parent().parent().parent().fadeOut();
-  } else if ($(this).hasClass('btn-minimize')) {
-    var $target = $(this).parent().parent().next('.card-block');
-    if (!$(this).hasClass('collapsed')) {
-      $('i',$(this)).removeClass($.panelIconOpened).addClass($.panelIconClosed);
-    } else {
-      $('i',$(this)).removeClass($.panelIconClosed).addClass($.panelIconOpened);
+    } else if ($(this).hasClass('btn-setting')) {
+      $('#myModal').modal('show');
     }
 
-  } else if ($(this).hasClass('btn-setting')) {
-    $('#myModal').modal('show');
+  });
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-});
+  function init(url) {
 
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
+    /* ---------- Tooltip ---------- */
+    $('[rel="tooltip"],[data-rel="tooltip"]').tooltip({"placement":"bottom",delay: { show: 400, hide: 200 }});
 
-function init(url) {
+    /* ---------- Popover ---------- */
+    $('[rel="popover"],[data-rel="popover"],[data-toggle="popover"]').popover();
 
-  /* ---------- Tooltip ---------- */
-  $('[rel="tooltip"],[data-rel="tooltip"]').tooltip({"placement":"bottom",delay: { show: 400, hide: 200 }});
+  }
+})
 
-  /* ---------- Popover ---------- */
-  $('[rel="popover"],[data-rel="popover"],[data-toggle="popover"]').popover();
-
-}
 
 document.addEventListener('turbolinks:load', function() {
   componentHandler.upgradeDom();
