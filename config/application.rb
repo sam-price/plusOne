f355f6f5593fa85cdfe27021020660ac623c1a73
sam-price <samprice.ethos@gmail.com>
2017-03-30 20:54:53 +0100

    Added will_paginate

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,5 @@
 require_relative 'boot'
-
 require 'rails/all'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -9,7 +7,6 @@ Bundler.require(*Rails.groups)
 module PlusOne
   class Application < Rails::Application
     # Added to allow for enum data type and schema show up
-
     # For the schema at the end - going to remove it for now
     # config.active_record.schema_format = :sql
   end
