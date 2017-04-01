647a880ff7405ba98dd141bc12fe1284d9fddd25
sam-price <samprice.ethos@gmail.com>
2017-04-02 00:39:17 +0100

    bug fixes

--- db/migrate/20170401225812_add_channel_key_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddChannelKeyToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :channel_key, :string
-  end
-end
--- db/migrate/20170401233639_drop_channel_key_from_users.rb
+++
@@ -1,5 +0,0 @@
-class DropChannelKeyFromUsers < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :users, :channel_key
-  end
-end
