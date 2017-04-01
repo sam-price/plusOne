a796d52e9ff2028607169f4d644862b0eb27d6da
sam-price <samprice.ethos@gmail.com>
2017-04-01 21:59:35 +0100

    Added conversation connection cable and channels

---
+++ app/assets/javascripts/channels/conversation.js
@@ -0,0 +1,31 @@
+// # App.conversation = App.cable.subscriptions.create "ConversationChannel",
+// #   connected: ->
+// #     # Called when the subscription is ready for use on the server
+// #
+// #   disconnected: ->
+// #     # Called when the subscription has been terminated by the server
+// #
+// #   received: (data) ->
+// #     # Called when there's incoming data on the websocket for this channel
+// #
+// #   speak: ->
+// #     @perform 'speak'
+
+App.conversation = App.cable.subscriptions.create("ConversationChannel", {
+  connected: function() {},
+  disconnected: function() {},
+  received: function(data) {
+    console.log(data['message']);
+  },
+  speak: function(message) {
+    return this.perform('speak', {
+      message: message
+    });
+  }
+});
+$(document).on('submit', '.new_message', function(e) {
+  e.preventDefault();
+  var values = $(this).serializeArray();
+  App.conversation.speak(values);
+  $(this).trigger('reset');
+});
