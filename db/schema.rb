0e6c77b7899006a40d83df83e47deea66e1ef98e
sam-price <samprice.ethos@gmail.com>
2017-04-03 22:01:41 +0100

    Added bio to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403192309) do
+ActiveRecord::Schema.define(version: 20170403205058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -117,6 +117,7 @@ ActiveRecord::Schema.define(version: 20170403192309) do
     t.string   "last_name"
     t.date     "date_of_birth"
     t.integer  "city_id"
+    t.text     "bio"
     t.index ["city_id"], name: "index_users_on_city_id", using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
